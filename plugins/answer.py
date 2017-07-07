db86611fb7aaeef1dbf25ab083e8e3d2967b144a
Meet Mangukiya <meetmangukiya98@gmail.com>
2017-07-07 21:20:02 +0530

    fix stuff

--- plugins/answer.py
+++ plugins/answer.py
@@ -2,14 +2,14 @@ from errbot import BotPlugin, botcmd
 
 from gensim.summarization import summarize
 
-from answers.final import get_answer
+from answers.final import get_answer, graph
 
 
 class Answer(BotPlugin):
 
     @botcmd
     def answer(self, msg, arg):
-        answers = list(get_answer(arg, final=True))
+        answers = list(get_answer(arg, graph, final=True))
         if answers:
             yield summarize(''.join(answers[0][0].splitlines(True)[-1]))
             doc_link = 'https://api.coala.io/en/latest/Developers/' + answers[0][0].splitlines()[-1]
