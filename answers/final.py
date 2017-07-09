464b5df7ebacf360a2559229d25e45565aa454f0
Meet Mangukiya <meetmangukiya98@gmail.com>
2017-07-09 06:16:19 +0530

    final: Add weighted scoring

--- answers/final.py
+++ answers/final.py
@@ -13,6 +13,30 @@ parse_docs()
 
 nlp = spacy.load('en_core_web_md')
 
+SCORES = {
+    "ADJ": 3,
+    "NOUN": 3,
+    "ADV": 2,
+    "VERB": 2,
+    "INTJ": 2,
+    "X": 2,
+    "PRON": 2,
+    "ADP": 2,
+    "AUX": 1,
+    "CONJ": 1,
+    "CCONJ": 1,
+    "SCONJ": 1,
+    "DET": 1,
+    "PROPN": 1,
+    "PART": 1,
+    "": 0,
+    "NUM": 0,
+    "PUNCT": 0,
+    "SYM": 0,
+    "EOL": 0,
+    "SPACE": 0
+}
+
 def grapheize(graph, doc, attrs={}):
     unallowed_tags = [
         'EX', 'HVS', 'MD', 'PDT',
@@ -25,19 +49,24 @@ def grapheize(graph, doc, attrs={}):
         nodes = [token.lemma_, token.head.lemma_]
         for node in nodes:
             if node not in graph:
-                graph.add_node(node, token=token)
+                graph.add_node(node, token=Counter())
             node = graph.node.get(node)
+            node['token'].update([token.pos_])
             for key, value in attrs.items():
                 node.setdefault(key, []).append(value)
         graph.add_edge(*nodes)
 
 def get_answer(question, graph, final=False):
+    global SCORES
+
     q_graph = networkx.Graph()
     q_doc = nlp(question)
     q_type = []
     for token in q_doc:
         if token.tag_.startswith('W'):
             q_type.append(token)
+
+    pos_dict = {a.string.strip(): a.pos_ for a in list(q_doc)}
 
     grapheize(q_graph, q_doc, attrs={'q_type': q_type})
     scores = Counter()
@@ -47,7 +76,13 @@ def get_answer(question, graph, final=False):
         if start in graph and end in graph:
             for path in networkx.algorithms.all_shortest_paths(graph, start, end):
                 for node in path:
-                    scores.update(graph.node.get(node)['text'])
+                    try:
+                        pos = graph.node.get(node)['token'].most_common()[0][0]
+                        score = SCORES[pos_dict.get(node, pos)]
+                        for text in graph.node.get(node)['text']:
+                            scores.update({text: score})
+                    except:
+                        logging.exception('Something went wrong: ')
 
     sorted_counter = OrderedDict(sorted(scores.items(), key=lambda x: x[1],
                                         reverse=True))
